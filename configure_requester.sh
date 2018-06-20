fb60d00c400bc845f587c8ee3bea7b2559e6d6fb
petems <p.morsou+GIT@gmail.com>
2018-06-20 13:23:31 +0100

    Adding GCP Terraform configuration
    * Allows setup of demo in GCP with Terraform code

--- configure_requester.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -v
-
-apt-get update -y
-
-apt-get install curl jq -y
-
--- scripts/ssh_to_vault_sad.sh
+++ scripts/ssh_to_vault_sad.sh
@@ -1,6 +1,4 @@
-
-
-export instance_id=$(terraform output vault_server_instance_id)
+export instance_id=$(terraform output vault_sad_instance_id)
 export project_id=$(terraform output project_id)
 
 gcloud compute ssh ${instance_id} --project ${project_id}
